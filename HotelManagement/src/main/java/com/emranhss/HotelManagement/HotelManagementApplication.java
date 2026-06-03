6d47df907d2c0970d19de52bddf0674bdaa23f26
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-06-03 18:33:51 +0600

    3

--- HotelManagement/src/main/java/com/emranhss/HotelManagement/HotelManagementApplication.java
+++ HotelManagement/src/main/java/com/emranhss/HotelManagement/HotelManagementApplication.java
@@ -7,11 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelManagementApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
-
-
-
-
 
 }
