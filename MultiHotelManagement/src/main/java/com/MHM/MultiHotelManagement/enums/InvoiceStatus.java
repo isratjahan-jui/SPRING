fbee0d3d9feecf1817a5c0a94cea18ef987c8d49
Israt Jahan Jui <isratjahanjui847@gmail.com>
2026-07-15 14:56:22 +0600

    add

--- MultiHotelManagement/src/main/java/com/MHM/MultiHotelManagement/enums/InvoiceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.MHM.MultiHotelManagement.enums;
-
-public enum InvoiceStatus {
-    ISSUED,     // Invoice তৈরি হয়েছে কিন্তু এখনো পরিশোধ হয়নি
-    PAID,       // Invoice সম্পূর্ণ পরিশোধ হয়েছে
-    CANCELLED   // Invoice বাতিল করা হয়েছে
-}
